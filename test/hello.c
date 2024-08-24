a2906d2d58fc65b9881f5296e893660ef63c93ae
qiushui999 <2276924443@qq.com>
2024-08-24 16:55:47 +0800

    first commit

---
+++ test/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "add.h"
+
+int main()
+{
+    int a=0;
+    int b=1;
+    int c=5;
+    a=add(b,c);
+    printf("%d\n",a);
+    printf("hello world\n");
+    return 0;
+
+}
